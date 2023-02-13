80f77a188333cee4548e8673eda756e380ab2eab
Sergei Golubev <svigolubev@gmail.com>
2023-02-13 22:47:14 +0530

    Lesson 5 	- Client connection to catalog was added

--- Assets/_Root/Scripts/PlayFab/PlayFabAccountManager.cs
+++ Assets/_Root/Scripts/PlayFab/PlayFabAccountManager.cs
@@ -1,24 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using PlayFab;
 using PlayFab.ClientModels;
-using System;
 
 
 public class PlayFabAccountManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _titleLable;
+    [SerializeField] private TMP_Text titleText;
 
     private void Start()
     {
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetInfo, OnError);
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
+    }
+
+    private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+    {
+        Debug.Log("CatalogSuccess");
+        ShowCatalog(result.Catalog);
+    }
+
+    private void ShowCatalog(List<CatalogItem> resultCatalog)
+    {
+        foreach (var item in resultCatalog)
+        {
+            Debug.Log($"{item.ItemId}");
+        }
     }
 
     private void OnGetInfo(GetAccountInfoResult obj)
     {
-        _titleLable.text = $"Player id: {obj.AccountInfo.PlayFabId}\n" +
+        titleText.text = $"Player id: {obj.AccountInfo.PlayFabId}\n" +
             $"Username: {obj.AccountInfo.Username}\n" +
             $"Created: {obj.AccountInfo.Created}";
     }
