5a84667eef174c70fc7321d59d82b9b495c8e963
Sergei Golubev <svigolubev@gmail.com>
2023-03-03 17:45:53 +0530

    Start point

--- Assets/_Root/Scripts/RoomCreator/NamesConfig.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Configs/" + nameof(NamesConfig), fileName = nameof(NamesConfig))]
-public sealed class NamesConfig : ScriptableObject
-{
-    [field: SerializeField] public string[] FirstName { get; private set; }
-    [field: SerializeField] public string[] SecondName { get; private set; }
-}
